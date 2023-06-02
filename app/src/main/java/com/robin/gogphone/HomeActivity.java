b184e1a6c9b9871b34657dbdfee26c019a52eea1
robijntje <44802556+robijntje@users.noreply.github.com>
2023-06-02 14:02:53 +0200

    Created an activity for the home screen
    Created an activity that contains the graph data for the home screen graph.

---
+++ app/src/main/java/com/robin/gogphone/HomeActivity.java
@@ -0,0 +1,41 @@
+package com.robin.gogphone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.robin.gogphone.databinding.ActivityMainBinding;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+      //  binding = ActivityMainBinding.inflate(getLayoutInflater());
+      //  setContentView(binding.getRoot());
+        setContentView(R.layout.fragment_home);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+    }
+
+}
